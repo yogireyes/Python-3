dea03aee792e2fb180e40d83d08d6fd555b006e0
yogireyes <yogireyes2@gmail.com>
2023-10-24 19:44:20 -0400

    Upload files

---
+++ myperfectmodule.py
@@ -0,0 +1,11 @@
+def calculate_square_footage(length, width):
+    """
+    Calculate the square footage of a house.
+
+    :param length: Length of the house in feet
+    :param width: Width of the house in feet
+    :return: Square footage (area) of the house
+    """
+    area = length * width
+    return area
+
---
+++ myperfectmodule2.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_circumference(radius):
+    """
+    Calculate the circumference of a circle.
+
+    :param radius: Radius of the circle
+    :return: Circumference of the circle
+    """
+    circumference = 2 * math.pi * radius
+    return circumference
